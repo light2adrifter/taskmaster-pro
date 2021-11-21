f19599b50f2fead77e0184741c9157ebc94f8a6f
light <light2adrifter@yahoo.com>
2021-11-21 01:08:06 -0500

    Added CSS for buttons and transition for 'trashbin', auto check timer for due dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,16 +53,22 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
+    // console.log("activate", this);
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
+    // console.log("deactivate", this);
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active")
+    // console.log("over", event.target);
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active")
+    // console.log("out", event.target);
   },
 
   update: function(event) {
@@ -190,6 +196,7 @@ $(".list-group").on("change", "input[type='text']", function() {
 
 // Due date checker
 var auditTask = function(taskEl) {
+  console.log(taskEl)
   // get date from task element
   var date = $(taskEl).find("span").text().trim();
   // ensure it worked
@@ -224,7 +231,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -265,13 +272,15 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
-    console.log("drop");
+    // console.log("drop");
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active")
+    // console.log("over");
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active")
+    // console.log("out");
   }
   
 });
@@ -284,6 +293,13 @@ $("#modalDueDate").datepicker({
     $(this).trigger("change");
   }
 });
+
+// interval check
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
